f9346c66cf13ad77dc0cbcd14f7db104acbaeb69
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2025-03-01 11:50:17 +0700

    The buttons now display the appropriate contents when clicked. Added things to webpack config to load image.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import { pageLoad } from "./PageLoad.js";
+import { loadMenu } from "./menu.js";
 
 pageLoad();
+
+const nav = document.querySelector("nav");
+
+nav.addEventListener("click", (e) => {
+    const target = e.target;
+    
+    if (target.tagName == "BUTTON") {
+        const contentDiv = document.querySelector("#content");
+        
+        while (contentDiv.firstChild) {
+            contentDiv.removeChild(contentDiv.firstChild);
+        }
+    }
+
+    if (target.textContent == "Home") {
+        pageLoad();
+    }
+    
+    if (target.textContent == "Menu") {
+        loadMenu();
+    }
+})
